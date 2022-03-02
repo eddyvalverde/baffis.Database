64e4857ec7e18f4f640e458f251f543ab1760b60
eddygarros <eddygarros@hotmail.com>
2022-03-02 11:23:23 -0600

    fix: fix sql

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,27 +1,32 @@
+
+
 CREATE TABLE Currency(
    IdCurrency INT PRIMARY KEY     NOT NULL,
    NAME           TEXT    NOT NULL,
-   COUNTRY           TEXT    NOT NULL,
+   COUNTRY           TEXT    NOT NULL
 );
 
 CREATE TABLE Subscription(
    IDSubscription INT PRIMARY KEY     NOT NULL,
    Title           TEXT    NOT NULL,
    Description           TEXT    NOT NULL,
-   Cost                 DOUBLE NOT NULL,
+   Cost                 money NOT NULL,
    IdCurrency INT NOT NULL,
    CONSTRAINT fk_currency
-      FOREIGN KEY(Currency) 
+      FOREIGN KEY(IdCurrency) 
 	  REFERENCES Currency(IdCurrency)
 );
 
-CREATE TABLE Order(
+CREATE TABLE Orders(
    IDSubscription INT  NOT NULL,
    Subscriber           TEXT    NOT NULL,
-   SubscribedOn           DATETIME    NOT NULL,
-   ExpiresOn                 DATETIME,
+   SubscribedOn           TIMESTAMP    NOT NULL,
+   ExpiresOn                 TIMESTAMP,
    PaymentDay   INT,
    Cost         MONEY,
-   PRIMARY KEY (Subscriber, IDSubscription)
+   PRIMARY KEY (Subscriber, IDSubscription),
+   CONSTRAINT fk_order
+      FOREIGN KEY(IDSubscription) 
+	  REFERENCES Subscription(IDSubscription)
 );
 
